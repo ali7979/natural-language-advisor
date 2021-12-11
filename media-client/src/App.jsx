b9e8c5c4be25189599d6b729293c3232e198c884
dreadwing5 <sachinspyk@outlook.com>
2021-12-12 01:26:19 +0530

    removed deprecation packages

--- media-client/src/App.jsx
+++ media-client/src/App.jsx
@@ -1,82 +1,109 @@
-import "./App.css";
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 
-function App() {
-  const [file, setFile] = React.useState(null);
-  const [files, setFiles] = React.useState([]);
-  const filehandler = (e) => {
-    if (e.target.files != null || e.target.files[0] != null) {
-      setFile(e.target.files[0]);
-    }
+const App = () => {
+  const [file, setFile] = useState(null);
+  const [inputContainsFile, setInputContainsFile] = useState(false);
+  const [currentlyUploading, setCurrentlyUploading] = useState(false);
+  const [videoId, setvideoId] = useState(null);
+  const [progress, setProgress] = useState(null);
+
+  const handleFile = (event) => {
+    setFile(event.target.files[0]);
+    setInputContainsFile(true);
   };
 
-  const uploadFile = async (e) => {
-    e.preventDefault();
-    if (file) {
-      const fd = new FormData();
-
-      fd.append("file", file);
-
-      const res = await axios.post("http://localhost:5000/upload", fd);
-
-      setFiles(files.concat(res.data));
-    }
+  const fileUploadHandler = () => {
+    const fd = new FormData();
+    fd.append("file", file, file.name);
+    axios
+      .post(`http://localhost:8000/api/video/upload`, fd, {
+        onUploadProgress: (progressEvent) => {
+          setProgress((progressEvent.loaded / progressEvent.total) * 100);
+          console.log(
+            "upload progress: ",
+            Math.round((progressEvent.loaded / progressEvent.total) * 100)
+          );
+        },
+      })
+      .then(({ data }) => {
+        setvideoId(data);
+        setFile(null);
+        setInputContainsFile(false);
+        setCurrentlyUploading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+        if (err.response.status === 400) {
+          const errMsg = err.response.data;
+          if (errMsg) {
+            console.log(errMsg);
+            alert(errMsg);
+          }
+        } else if (err.response.status === 500) {
+          console.log("db error");
+          alert("db error");
+        } else {
+          console.log("other error: ", err);
+        }
+        setInputContainsFile(false);
+        setCurrentlyUploading(false);
+      });
   };
 
-  const fetchFiles = React.useCallback(async () => {
-    const res = await axios.get("http://localhost:5000/files");
-    setFiles(res.data);
-  }, []);
-
-  const removeFile = React.useCallback(
-    async (filename, index) => {
-      const res = await axios.delete(
-        `http://localhost:5000/delete/${filename}`
-      );
-      if (res.status === 200) {
-        let temp = [...files];
-        console.log(temp);
-        temp.splice(index, 1);
-
-        setFiles(temp);
-      }
-    },
-    [files]
-  );
-
-  React.useEffect(() => {
-    fetchFiles();
-  }, [fetchFiles]);
-
+  const handleClick = () => {
+    if (inputContainsFile) {
+      setCurrentlyUploading(true);
+      fileUploadHandler();
+    }
+  };
   return (
-    <div className="App">
-      <form className="Form" onSubmit={uploadFile}>
-        <input type="file" onChange={filehandler} />
-        <button type="submit">upload</button>
-      </form>
-      <div className="Media">
-        {files.map((file, i) => (
-          <div key={file._id} className="Item">
+    <div className="regular">
+      <div className="video-section">
+        {videoId ? (
+          <>
             <video id="videoPlayer" width="650" controls muted="muted" autoplay>
               <source
-                src={`http://localhost:5000/read/${file.filename}`}
+                src={`http://localhost:8000/api/video/${videoId}`}
                 type="video/mp4"
               />
             </video>
-            <button
-              type="button"
-              onClick={() => {
-                removeFile(file.filename, i);
-              }}
+            <a
+              className="link"
+              href={`http://localhost:8000/api/video/${videoId}`}
+              target="_blank"
             >
-              remove
-            </button>
-          </div>
-        ))}
+              link
+            </a>
+          </>
+        ) : (
+          <p className="nopic">no regular version pic yet</p>
+        )}
+      </div>
+      <div className="inputcontainer">
+        {currentlyUploading ? (
+          <p>uploading...</p>
+        ) : (
+          <>
+            <input
+              className="file-input"
+              onChange={handleFile}
+              type="file"
+              name="file"
+              id="file"
+            />
+            <label
+              className={`inputlabel ${file && "file-selected"}`}
+              htmlFor="file"
+              onClick={handleClick}
+            >
+              {file ? <>SUBMIT</> : <>REGULAR VERSION</>}
+            </label>
+          </>
+        )}
       </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ routes/video.js
@@ -0,0 +1,114 @@
+const mongoose = require("mongoose");
+const { GridFsStorage } = require("multer-gridfs-storage");
+const router = require("express").Router();
+const multer = require("multer");
+const crypto = require("crypto");
+const path = require("path");
+require("dotenv").config();
+
+const mongoURI = process.env.MONGO_URI;
+const conn = mongoose.createConnection(mongoURI);
+
+let gfs;
+conn.once("open", () => {
+  gfs = new mongoose.mongo.GridFSBucket(conn.db, {
+    bucketName: "media",
+  });
+});
+
+const storage = new GridFsStorage({
+  url: mongoURI,
+  options: { useUnifiedTopology: true },
+  file: (req, file) => {
+    // this function runs every time a new file is created
+    return new Promise((resolve, reject) => {
+      // use the crypto package to generate some random hex bytes
+      crypto.randomBytes(16, (err, buf) => {
+        if (err) {
+          return reject(err);
+        }
+        // turn the random bytes into a string and add the file extention at the end of it (.png or .jpg)
+        // this way our file names will not collide if someone uploads the same file twice
+        const filename = buf.toString("hex") + path.extname(file.originalname);
+        const fileInfo = {
+          filename: filename,
+          bucketName: "media",
+        };
+        // resolve these properties so they will be added to the new file document
+        resolve(fileInfo);
+      });
+    });
+  },
+});
+
+// set up our multer to use the gridfs storage defined above
+const store = multer({
+  storage,
+  // limit the size to 20mb for any files coming in
+  // filer out invalid filetypes
+  fileFilter: function (req, file, cb) {
+    checkFileType(file, cb);
+  },
+});
+
+function checkFileType(file, cb) {
+  const filetypes = /mp4|mov|mkv/;
+  //check the file extention
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+  // more importantly, check the mimetype
+  const mimetype = filetypes.test(file.mimetype);
+  // if both are good then continue
+  if (mimetype && extname) return cb(null, true);
+  // otherwise, return error message
+  cb("filetype");
+}
+
+const uploadMiddleware = (req, res, next) => {
+  const upload = store.single("file");
+  upload(req, res, function (err) {
+    if (err) {
+      // check if our filetype error occurred
+      if (err === "filetype") return res.status(400).send("Video files only");
+      // An unknown error occurred when uploading.
+      console.log(err);
+      return res.sendStatus(500);
+    }
+    // all good, proceed
+    next();
+  });
+};
+
+router.post("/upload", uploadMiddleware, async (req, res) => {
+  const { file } = req;
+  const { id } = file;
+  console.log("uploaded file: ", file);
+  return res.send(file.id);
+});
+
+const deleteVideo = (id) => {
+  if (!id || id === "undefined") return res.status(400).send("no image id");
+  const _id = new mongoose.Types.ObjectId(id);
+  gfs.delete(_id, (err) => {
+    if (err) return res.status(500).send("video deletion error");
+  });
+};
+
+// this route will be accessed by any img tags on the front end which have
+// src tags like
+// <img src="/api/video/123456789" alt="example"/>
+// <img src={`/api/image/${user.profilePic}`} alt="example"/>
+router.get("/:id", ({ params: { id } }, res) => {
+  // if no id return error
+  if (!id || id === "undefined") return res.status(400).send("no video id");
+  // if there is an id string, cast it to mongoose's objectId type
+  const _id = new mongoose.Types.ObjectId(id);
+  // search for the image by id
+  gfs.find({ _id }).toArray((err, files) => {
+    if (!files || files.length === 0)
+      return res.status(400).send("no files exist");
+    // if a file exists, send the data
+    gfs.openDownloadStream(_id).pipe(res);
+  });
+});
+
+module.exports = router;
